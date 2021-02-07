ecbf4b1731e2eec7b331b8ac94f9c7fd7e6db148
Akash Joshi <infinityakash94@gmail.com>
2021-02-07 15:21:41 -0700

    Added Rating and Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,28 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
+
 
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">Skymart</a>
+          <a className="brand" href="/">Skymart</a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
             {
-              data.products.map(product => (
-                <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-
-                    <img className="medium" src={product.image} alt={product.name} />
-                  </a>
-                  <div className="card-body">
-                   
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+              data.products.map(product => (              
+                <Product product={product} key = {product._id} ></Product>
               ))
             }
 
